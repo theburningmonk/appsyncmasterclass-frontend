a9c0f8e50192e0f422dc0497f85cfd3c9ead85e1
Gerard Sans <gsans@amazon.com>
2021-01-20 16:54:32 +0000

    Setup and test User Pool using Amplify UI

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,43 @@
 <template>
   <div id="app">
-    <i class="fas fa-thumbs-up fa-5x"></i>
+    <amplify-authenticator usernameAlias="email">
+      <amplify-sign-up
+        slot="sign-up"
+        :form-fields.prop="signUpFields"
+      ></amplify-sign-up>
+      <div>
+        <h1 v-if="user && user.attributes">Hey, {{user.attributes.name}}!</h1>
+        <amplify-sign-out/>
+      </div>
+    </amplify-authenticator>
   </div>
 </template>
 
 <script>
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components'
 export default {
   name: 'App',
+  data() {
+    return {
+      user: { },
+      signUpFields: [
+        { type: 'username', label: 'Username (Email) *', required: true },
+        { type: 'password', label: 'Password *', required: true },
+        { type: 'name', label: 'Name *', required: true },
+        { type: 'phone_number', label: 'Phone Number *', required: true },
+      ]
+    }
+  },
+  created() {
+    // authentication state management
+    onAuthUIStateChange((state, user) => {
+      // set current user and load data after login
+      if (state === AuthState.SignedIn) {
+        this.user = user;
+        console.log(user);
+      }
+    })
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
+import Amplify from 'aws-amplify';
+import '@aws-amplify/ui-vue';
+
+Amplify.configure({
+  Auth: {
+    region: 'eu-west-1',
+    userPoolId: 'eu-west-1_G0S2EuViF',
+    userPoolWebClientId: '2elpvum0k1l6qpf0iqgeh0jh4q',
+    mandatorySignIn: true
+  }
+})
+
+const myAppConfig = {
+  'aws_appsync_graphqlEndpoint': 'https://t6fywejiqffdjdwlbrslhhzg3e.appsync-api.eu-west-1.amazonaws.com/graphql',
+  'aws_appsync_region': 'eu-west-1',
+  'aws_appsync_authenticationType': 'AMAZON_COGNITO_USER_POOLS'
+}
+
+Amplify.configure(myAppConfig);
 
 Vue.config.productionTip = false
 
