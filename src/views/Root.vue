2b390b6f26b5fc2ba16c382cdf8b7884943a862b
Gerard Sans <gerard.sans@gmail.com>
2021-01-29 16:07:06 +0000

    Remove amplify auth flow test

--- src/views/Root.vue
+++ src/views/Root.vue
@@ -1,38 +1,11 @@
 <template>
   <div>
     <h1>Root</h1>
-    <amplify-authenticator usernameAlias="email">
-      <amplify-sign-out/>
-    </amplify-authenticator>
   </div>
 </template>
 
 <script>
-import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
-import { mapActions, mapGetters } from 'vuex';
-
 export default {
   name: 'Root',
-  computed: {
-    ...mapGetters('authentication', [
-      'loggedIn',
-      'user'
-    ]),
-  },
-  methods: {
-    ...mapActions('authentication', [
-      'loginUser',
-      'logoutUser'
-    ]),
-  },
-  created() {
-    onAuthUIStateChange(async (state, user) => {
-      if (state === AuthState.SignedIn) {
-        await this.loginUser(user);
-      } if (state === AuthState.SignedOut) {
-        await this.logoutUser();
-      }
-    })
-  }
 }
 </script>
