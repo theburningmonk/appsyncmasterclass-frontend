a7bcfd6e32cd5e0bfca46996990880fb2cf2d565
Gerard Sans <gerard.sans@gmail.com>
2021-01-29 16:21:02 +0000

    Setup Root base styling

--- src/views/Root.vue
+++ src/views/Root.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <h1>Root</h1>
+  <div class="flex w-full h-screen">
+    <!-- left side -->
+    <div class="flex w-1/2 h-full bg-blue">Left</div>
+
+    <!-- right side -->
+    <div class="flex w-1/2 h-full">Right</div>
   </div>
 </template>
 
@@ -9,3 +13,13 @@ export default {
   name: 'Root',
 }
 </script>
+
+<style>
+div {
+  border: 1px dashed blue;
+  padding: 15px;
+}
+div.items-center.justify-center {
+  background-color: rgba(240, 240, 240, 0.5);
+}
+</style>
