0d7aa69474abeba228f4794444bec6f6d0a65455
Gerard Sans <gerard.sans@gmail.com>
2021-01-29 16:06:23 +0000

    Setup tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    container: {
+      center: true
+    },
+    extend: {
+      fontFamily: {
+        sans: [...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        'blue': '#1DA1F2',
+        'darkblue': '#2795D9',
+        'lightblue': '#EFF9FF',
+        'dark': '#657786',
+        'light': '#AAB8C2',
+        'lighter': '#E1E8ED',
+        'lightest': '#F5F8FA',
+      }
+    },
   },
   variants: {
     extend: {},
