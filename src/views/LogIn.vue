0a041ee55a7b2267fa33bae9de884d8a03cff79c
Gerard Sans <gerard.sans@gmail.com>
2021-04-26 18:46:35 +0100

    Add Amplify sign in

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -10,11 +10,13 @@
       <p class="text-dark">Password</p>
       <input v-model="password" class="w-full bg-lightblue text-lg" type="password">
     </div>
-    <button class="w-1/3 font-bold rounded-full bg-blue text-white p-3 pl-3 pr-3 hover:bg-darkblue">Log in</button>
+    <button @click.prevent="signIn" class="w-1/3 font-bold rounded-full bg-blue text-white p-3 pl-3 pr-3 hover:bg-darkblue">Log in</button>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
   name: 'LogIn',
   data() {
@@ -25,6 +27,21 @@ export default {
   },
   created: function() {
     this.$nextTick(() => this.$refs.email.focus())
+  },
+  methods: {
+    ...mapActions('authentication', [
+      'signInUser',
+      'logoutUser',
+    ]),
+    async signIn() {
+      try {
+        await this.signInUser({ email: this.email, password: this.password });
+      } catch (error) {
+        this.logoutUser();
+        alert('Error signing in, please check console for error detail')
+        console.log('error signing in', error);
+      }
+    },
   }
 }
 </script>
