a368fe5c4c1425565e8ec124542cbdd74904c1e3
Gerard Sans <gerard.sans@gmail.com>
2021-06-09 19:49:57 +0100

    Setup Home base styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <h1>Home</h1>
+  <div id="app" class="flex container h-screen w-full">
+    <!-- side navigation -->
+    <div class="lg:w-1/5 border-r border-lighter px-2 lg:px-6 py-2 flex flex-col justify-between">
+      Side Navigation
+    </div>
+
+    <!-- tweets -->
+    <div class="w-1/2 h-full overflow-y-scroll">
+      Tweets
+    </div>
+
+    <!-- default right bar -->
+    <div class="hidden md:block w-1/3 h-full border-l border-lighter px-6 py-2 overflow-y-scroll relative">
+      Right Bar
+    </div>
   </div>
 </template>
 
@@ -9,3 +22,13 @@ export default {
   name: 'Home'
 }
 </script>
+
+<style>
+div {
+  border: 1px dashed blue;
+  padding: 15px;
+}
+div.items-center.justify-center {
+  background-color: rgba(240, 240, 240, 0.5);
+}
+</style>
