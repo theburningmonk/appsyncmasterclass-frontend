c78f353464280b21ad50eb6be8193e10568904ab
Gerard Sans <gerard.sans@gmail.com>
2021-07-22 15:04:40 +0100

    Refactor timeline as a component

---
+++ src/components/Tweets.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div v-for="tweet in tweets" :key="tweet.id">
+      <div class="w-full p-4 border-b hover:bg-lightest flex">
+        <div class="flex-none mr-4">
+          <img :src="`${tweet.profile.imageUrl || 'default_profile.png'}`" class="h-12 w-12 rounded-full flex-none"/>
+        </div>
+        <div class="w-full">
+          <div class="flex items-center w-full">
+            <p class="font-semibold">{{ tweet.profile.name }}</p>
+            <p class="text-sm text-dark ml-2">@{{ tweet.profile.screenName }}</p>
+            <p class="text-sm text-dark ml-2">{{ tweet.time }}</p>
+            <i class="fas fa-angle-down text-sm ml-auto"></i>
+          </div>
+          <p v-if="tweet.inReplyToUser" class="text-dark">
+            Replying to @{{ tweet.inReplyToUser.screenName }}
+          </p>
+          <p class="pb-2">
+            {{ tweet.text }}
+          </p>
+          <div class="flex w-full">
+            <div class="flex items-center text-sm text-dark w-1/4">
+              <button class="mr-2 rounded-full hover:bg-lighter">
+                <i class="far fa-comment"></i>
+              </button>
+              <p v-if="tweet.replies > 0"> {{ tweet.replies }} </p>
+            </div>
+            <div class="flex items-center text-sm text-dark w-1/4">
+              <button class="mr-2 rounded-full hover:bg-lighter">
+                <i :class="`fas fa-retweet ${ tweet.retweeted ? 'text-green-500' : '' }`"></i>
+              </button>
+              <p v-if="tweet.retweets > 0"> {{ tweet.retweets }} </p>
+            </div>
+            <div class="flex items-center text-sm text-dark w-1/4">
+              <button class="mr-2 rounded-full hover:bg-lighter">
+                <i :class="`fas fa-heart ${ tweet.liked ? 'text-red-600' : '' }`"></i>
+              </button>
+              <p v-if="tweet.likes > 0"> {{ tweet.likes }} </p>
+            </div>
+            <div class="flex items-center text-sm text-dark w-1/4">
+              <i class="fas fa-share-square mr-3"></i>
+            </div>
+          </div>
+        </div>
+      </div>            
+    </div>
+  </div>  
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'Tweets',
+  computed: {
+    ...mapGetters('twitter', {
+      nextToken: 'nextTokenTweets'
+    }),
+    ...mapGetters('twitter', [
+      'profile', 
+      'tweets', 
+    ]),    
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -36,52 +36,7 @@
           </button>
         </div>
 
-        <div>
-          <div v-for="tweet in tweets" :key="tweet.id">
-            <div class="w-full p-4 border-b hover:bg-lightest flex">
-              <div class="flex-none mr-4">
-                <img :src="`${tweet.profile.imageUrl || 'default_profile.png'}`" class="h-12 w-12 rounded-full flex-none"/>
-              </div>
-              <div class="w-full">
-                <div class="flex items-center w-full">
-                  <p class="font-semibold">{{ tweet.profile.name }}</p>
-                  <p class="text-sm text-dark ml-2">@{{ tweet.profile.screenName }}</p>
-                  <p class="text-sm text-dark ml-2">{{ tweet.time }}</p>
-                  <i class="fas fa-angle-down text-sm ml-auto"></i>
-                </div>
-                <p v-if="tweet.inReplyToUser" class="text-dark">
-                  Replying to @{{ tweet.inReplyToUser.screenName }}
-                </p>
-                <p class="pb-2">
-                  {{ tweet.text }}
-                </p>
-                <div class="flex w-full">
-                  <div class="flex items-center text-sm text-dark w-1/4">
-                    <button class="mr-2 rounded-full hover:bg-lighter">
-                      <i class="far fa-comment"></i>
-                    </button>
-                    <p v-if="tweet.replies > 0"> {{ tweet.replies }} </p>
-                  </div>
-                  <div class="flex items-center text-sm text-dark w-1/4">
-                    <button class="mr-2 rounded-full hover:bg-lighter">
-                      <i :class="`fas fa-retweet ${ tweet.retweeted ? 'text-green-500' : '' }`"></i>
-                    </button>
-                    <p v-if="tweet.retweets > 0"> {{ tweet.retweets }} </p>
-                  </div>
-                  <div class="flex items-center text-sm text-dark w-1/4">
-                    <button class="mr-2 rounded-full hover:bg-lighter">
-                      <i :class="`fas fa-heart ${ tweet.liked ? 'text-red-600' : '' }`"></i>
-                    </button>
-                    <p v-if="tweet.likes > 0"> {{ tweet.likes }} </p>
-                  </div>
-                  <div class="flex items-center text-sm text-dark w-1/4">
-                    <i class="fas fa-share-square mr-3"></i>
-                  </div>
-                </div>
-              </div>
-            </div>            
-          </div>
-        </div>
+        <Tweets />
           
       </div>
     </div>
@@ -94,13 +49,15 @@
 <script>
 import SideNav from '../components/SideNav.vue'
 import DefaultRightBar from '../components/DefaultRightBar.vue'
+import Tweets from '../components/Tweets.vue'
 import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'Home',
   components: {
     SideNav,
-    DefaultRightBar
+    DefaultRightBar,
+    Tweets
   },
   data() {
     return {
@@ -110,9 +67,6 @@ export default {
     }
   },
   computed: {
-    ...mapGetters('twitter', {
-      nextToken: 'nextTokenTweets'
-    }),
     ...mapGetters('twitter', [
       'profile', 
       'tweets', 
