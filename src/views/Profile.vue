ebab791fcb936ecd2af485155427eb6fa3f3710f
Gerard Sans <gerard.sans@gmail.com>
2021-08-04 16:36:13 +0100

    Add Profile page with base styling

---
+++ src/views/Profile.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app" class="flex container h-screen w-full">
+    <div class="flex container h-screen w-full">
+      <SideNav />
+
+      <div class="w-1/2 h-full overflow-y-scroll">
+        Profile
+      </div>
+
+      <div class="hidden md:block w-1/3 z-0 h-full border-l border-lighter px-6 py-2 overflow-y-scroll relative">
+        <SearchBar/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SideNav from '../components/SideNav.vue'
+import SearchBar from '../components/SearchBar.vue'
+export default {
+  name: 'Profile',
+  components: {
+    SideNav,
+    SearchBar
+  },
+}
+</script>
+
+<style>
+div {
+  border: 1px dashed blue;
+  padding: 15px;
+}
+div.items-center.justify-center {
+  background-color: rgba(240, 240, 240, 0.5);
+}
+</style>
