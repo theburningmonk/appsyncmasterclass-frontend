212fff472fd81c1b253e0087175a5a9f45507233
Gerard Sans <gerard.sans@gmail.com>
2021-08-04 16:41:42 +0100

    Add navigation to SideNav component

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -7,6 +7,7 @@
       <div>
         <button 
           v-for="tab in tabs" :key="tab.id"
+          @click="open(tab.target)"
           class="focus:outline-none hover:text-blue flex items-center px-4 py-2 hover:bg-lightblue rounded-full mr-auto mb-3">
           <i class="text-2xl mr-4" :class="tab.icon"></i>
           <p class="text-lg font-semibold text-left hidden lg:block"> {{ tab.title }}</p>
@@ -75,6 +76,17 @@ export default {
     ...mapActions('authentication', [
       'logoutUser',
     ]),
+    async open(target) {
+      const current = this.$route.name;
+      if (target != current || this.$route.params.screenName != this.profile.screenName) {
+        this.$router.push({
+          name: target,
+          params: {
+            screenName: this.profile.screenName,
+          }
+        })
+      }
+    },
     async signOut() {
       try {
         console.log('signing out...')
