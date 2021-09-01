51f7950a530e7efb8774374458c18d38d79e9bf1
Gerard Sans <gerard.sans@gmail.com>
2021-09-01 22:14:49 +0100

    Add followers and following user details

---
+++ src/components/User.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="w-full px-4 py-2 border-b hover:bg-lightest flex flex-row">
+    <div class="flex-none">
+      <a :href="`#/${user.screenName}`">
+        <img :src="`${user.imageUrl || 'default_profile.png'}`" class="h-12 w-12 rounded-full"/>
+      </a>
+    </div>
+    <div class="ml-2 flex flex-col w-full">
+      <div class="flex flex-row justify-between w-full">
+        <div class="flex flex-col">
+          <p class="font-bold">{{ user.name }}</p>
+          <p class="text-dark text-sm">@{{ user.screenName }}</p>
+        </div>
+        <div v-if="profile.id !== user.id">
+          <button v-if="!user.following"
+                  @click="followUser()" 
+                  class="ml-auto text-blue font-bold px-4 py-1 rounded-full border border-blue mb-2 hover:bg-lightblue">
+            Follow
+          </button>
+          <button v-if="user.following"
+                  @mouseover="followingLabel='Unfollow'"
+                  @mouseleave="followingLabel='Following'"
+                  @click="unfollowUser()"
+                  class="ml-auto text-white bg-blue font-bold px-4 py-1 rounded-full border mb-2 hover:bg-red-700">
+            {{ followingLabel }}
+          </button>
+        </div>
+      </div>
+      <div>
+        <p>{{ user.bio }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+export default {
+  name: 'User',
+  props: ['user'],
+  computed: {
+    ...mapGetters('twitter', [
+      'profile',
+    ]),
+  },
+  methods: {
+    ...mapActions('twitter', { 
+      follow: 'followUser',
+      unfollow: 'unfollowUser',
+    }),
+    async followUser() {
+      this.user.following = true
+      await this.follow(this.user.id).catch(err => {
+        console.error(`failed to follow [${this.user.id}]`, err)
+        this.user.following = false
+      })
+    },
+    async unfollowUser() {
+      this.user.following = false
+      await this.unfollow(this.user.id).catch(err => {
+        console.error(`failed to unfollow [${this.user.id}]`, err)
+        this.user.following = true
+      })
+    },
+  },
+  data() {
+    return {
+      followingLabel: 'Following'
+    }
+  }
+}
+</script>
--- src/components/Users.vue
+++ src/components/Users.vue
@@ -2,14 +2,19 @@
   <div>
     <span v-if="users.length === 0">No results</span>
     <div v-bind:key="user.id" v-for="user in users">
-      <div>{{ user.id }}</div>
+      <User :user="user"/>
     </div>
   </div>
 </template>
 
 <script>
+import User from './User.vue';
+
 export default {
   name: 'Users',
   props: ['users'],
+  components: {
+    User,
+  }
 }
 </script>
