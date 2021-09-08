0f1a5f21f318f50bf2681c3b703192534df6030b
Gerard Sans <gerard.sans@gmail.com>
2021-09-08 12:25:48 +0200

    Add scroll directive

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,7 +1,9 @@
 import escape from './escape.directive';
+import scroll from './scroll.directive';
 
 export default {
   install(Vue) {
     Vue.directive('escape', escape);
+    Vue.directive('scroll', scroll);
   }
 }
---
+++ src/directives/scroll.directive.js
@@ -0,0 +1,20 @@
+export default {
+  bind: function (el, binding) {
+    if (typeof binding.value !== 'function') return;
+    el.__callback__ = (event) => {
+      if (binding.arg == 'bottom') {
+        if (!el) return;
+        const isBottom = Math.ceil(el.offsetHeight + el.scrollTop) >= el.scrollHeight;
+        if (!isBottom) return;
+        binding.value(event, el);
+      } else {
+        binding.value(event, el);
+      }
+    }
+    el.addEventListener('scroll', el.__callback__);
+  },
+  unbind: function (el) {
+    el.removeEventListener('scroll', el.__callback__);
+    el.__callback__ = null;
+  }
+}
