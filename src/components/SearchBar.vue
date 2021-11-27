a43a0f9cddf131e80d47929c9eb5d746bfaa5c02
Gerard Sans <gerard.sans@gmail.com>
2021-11-27 12:30:22 +0000

    Update Search bar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,12 +1,29 @@
 <template>
   <div>
     <i class="fas fa-search absolute mt-2 ml-5 text-sm text-light"></i>
-    <input class="pl-12 rounded-full w-full p-2 bg-lighter text-sm mb-4" placeholder="Search Tweet"/>
+    <input class="pl-12 rounded-full w-full p-2 bg-lighter text-sm mb-4"
+      placeholder="Search Tweet"
+      v-model="query"
+      v-on:keyup.enter="submit()"
+    />
   </div>
 </template>
 
 <script> 
 export default {
-  name: 'SearchBar'
+  name: 'SearchBar',
+  data() {
+    return {
+      query: '',
+    }
+  },
+  methods: {
+    submit() {
+      this.$router.push({  
+        name: 'Search',
+        query: { q: this.query } 
+      });
+    }
+  },
 }
 </script>
