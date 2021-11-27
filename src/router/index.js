0a85e4c4e10ff9f35625b82a534cf706d1cedc81
Gerard Sans <gerard.sans@gmail.com>
2021-11-27 12:28:50 +0000

    Add search route

--- src/router/index.js
+++ src/router/index.js
@@ -45,6 +45,15 @@ const routes = [
     meta: { protected: true }
   },
   {
+    path: '/search',
+    name: 'Search',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "search" */ '../views/Search.vue'),
+    meta: { protected: true }
+  },
+  {
     path: '/:screenName',
     name: 'Profile',
     // route level code-splitting
