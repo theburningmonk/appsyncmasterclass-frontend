9df03bd8b53489d0468ccc86aa15b69df9011ec4
Gerard Sans <gerard.sans@gmail.com>
2021-12-07 20:03:03 +0000

    Update Side Navigation

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,31 +1,61 @@
 <template>
-  <div class="lg:w-1/5 border-r border-lighter px-2 lg:px-6 py-2 flex flex-col justify-between min-w-max">
+  <div class="w-1/6 md:w-1/12 xl:w-1/6 border-r border-lighter lg:px-6 py-2 flex-col justify-between hidden sm:flex">
     <div>
       <button class="h-12 w-12 hover:bg-lightblue text-3xl text-blue rounded-full">
-        <i class="fab fa-twitter"></i>
+        <i class="ml-2 fab fa-twitter"></i>
       </button>
       <div>
-        <button 
-          v-for="tab in tabs" :key="tab.id"
-          @click="open(tab.target)"
-          class="focus:outline-none hover:text-blue flex items-center px-4 py-2 hover:bg-lightblue rounded-full mr-auto mb-3">
-          <i class="text-2xl mr-4" :class="tab.icon"></i>
-          <p class="text-lg font-semibold text-left hidden lg:block"> {{ tab.title }}</p>
+        <button @click="open('Home')" class="focus:outline-none hover:text-blue flex items-center px-4 py-2 hover:bg-transparent md:hover:bg-lightblue rounded-full mr-auto mb-1">
+          <i class="fa fa-home" :class="$route.name == 'Home'?'text-2xl':'text-xl'" aria-hidden="true"></i>
+          <p class="text-lg ml-4 text-left hidden xl:block" :class="$route.name == 'Home'?'font-bold':''"> Home</p>
+        </button>
+        <button class="focus:outline-none hover:text-blue flex items-center px-4 py-2 hover:bg-transparent md:hover:bg-lightblue rounded-full mr-auto mb-1">
+          <i class="fas fa-hashtag" aria-hidden="true"></i>
+          <p class="text-lg ml-4 text-left hidden xl:block"> Explore</p>
+        </button>
+        <button @click="open('Notifications')" class="relative focus:outline-none hover:text-blue flex items-center px-4 py-2 hover:bg-transparent md:hover:bg-lightblue rounded-full mr-auto mb-1">
+          <i class="fa-bell" :class="$route.name == 'Notifications'?'text-2xl fas':'text-xl far'" aria-hidden="true"></i>
+          <span v-if="this.newNotifications > 0" class="absolute -mt-6 ml-2">
+            <div class="inline-flex items-center px-1.5 py-0.5 border-2 border-white rounded-full text-xs font-semibold leading-4 bg-blue text-white">
+              {{ this.newNotifications }}
+            </div>
+          </span>
+          <p class="text-lg ml-4 text-left hidden xl:block" :class="$route.name == 'Notifications'?'font-bold':''"> Notifications</p>
+        </button>
+        <button class="focus:outline-none hover:text-blue flex items-center px-4 py-2 hover:bg-transparent md:hover:bg-lightblue rounded-full mr-auto mb-1">
+          <i class="text-xl far fa-envelope" aria-hidden="true"></i>
+          <p class="text-lg ml-4 text-left hidden xl:block"> Messages</p>
+        </button>
+        <button class="focus:outline-none hover:text-blue flex items-center px-4 py-2 hover:bg-transparent md:hover:bg-lightblue rounded-full mr-auto mb-1">
+          <i class="text-xl far fa-bookmark" aria-hidden="true"></i>
+          <p class="text-lg ml-4 text-left hidden xl:block"> Bookmarks</p>
+        </button>
+        <button class="focus:outline-none hover:text-blue flex items-center px-4 py-2 hover:bg-transparent md:hover:bg-lightblue rounded-full mr-auto mb-1">
+          <i class="text-xl fas fa-clipboard-list" aria-hidden="true"></i>
+          <p class="text-lg ml-4 text-left hidden xl:block"> Lists</p>
+        </button>
+        <button @click="open('Profile')" class="focus:outline-none hover:text-blue flex items-center px-4 py-2 hover:bg-transparent md:hover:bg-lightblue rounded-full mr-auto mb-1">
+          <i class="fa-user" :class="$route.name == 'Profile'?'text-2xl fas':'text-xl far'" aria-hidden="true"></i>
+          <p class="text-lg ml-4 text-left hidden xl:block" :class="$route.name == 'Profile'?'font-bold':''"> Profile</p></button>
+        <button class="focus:outline-none hover:text-blue flex items-center px-4 py-2 hover:bg-transparent md:hover:bg-lightblue rounded-full mr-auto mb-1">
+          <i class="text-xl fas fa-ellipsis-h" aria-hidden="true"></i>
+          <p class="text-lg ml-4 text-left hidden xl:block"> More</p>
         </button>
       </div>
-      <button class="text-white bg-blue rounded-full font-semibold h-12 lg:h-auto w-20 lg:w-full p-3 hover:bg-darkblue">
-        <p class="hidden lg:block">Tweet</p>
-        <i class="fas fa-plus lg:hidden"></i>
+      
+      <button @click="open('Home')" class="text-white bg-blue rounded-full font-semibold h-12 lg:h-auto w-12 xl:w-full hover:bg-darkblue p-3">
+        <p class="hidden xl:block">Tweet</p>
+        <i class="fas fa-plus xl:hidden"></i>
       </button>
     </div>
-    <div class="lg:w-full relative">
-      <button @click="dropdown = !dropdown" class="flex items-center w-full hover:bg-lightblue rounded-full p-2">
+    <div class="xl:w-full relative mt-2 w-10 h-10">
+      <button @click="dropdown = !dropdown" class="flex items-center w-full hover:bg-lightblue rounded-full">
         <img :src="`${profile.imageUrl || 'default_profile.png'}`" class="w-10 h-10 rounded-full" />
-        <div class="hidden lg:block ml-4 truncate">
+        <div class="hidden xl:block ml-4 truncate">
           <div class="text-left text-sm font-bold leading-tight truncate">{{ profile.name }}</div>
           <div class="text-left text-sm leading-tight text-dark truncate">{{ profile.ScreenName }}</div>
         </div>
-        <i class="hidden lg:block fas fa-angle-down ml-auto text-lg"></i>
+        <i class="hidden xl:block fas fa-angle-down ml-auto text-lg"></i>
       </button>
       <div v-if="dropdown === true" class="absolute bottom-0 left-0 w-64 rounded-lg shadow-md border-lightest bg-white mb-16">
         <button @click="dropdown = false" class="p-3 flex items-center w-full hover:bg-lightest">
@@ -49,27 +79,17 @@
 
 <script>
 import { mapActions, mapGetters } from 'vuex';
-
 export default {
   name: 'SideNav',
   data() {
     return {
-      tabs: [
-        {icon: 'fas fa-home', title: 'Home', id: 'home', target: 'Home'},
-        {icon: 'fas fa-hashtag', title: 'Explore', id: 'explore'},
-        {icon: 'far fa-bell', title: 'Notifications', id: 'notifications'},
-        {icon: 'far fa-envelope', title: 'Messages', id: 'messages'},
-        {icon: 'far fa-bookmark', title: 'Bookmarks', id: 'bookmarks'},
-        {icon: 'fas fa-clipboard-list', title: 'Lists', id: 'lists'},
-        {icon: 'far fa-user', title: 'Profile', id: 'profile', target: 'Profile'},
-        {icon: 'fas fa-ellipsis-h', title: 'More', id: 'more'}
-      ],
       dropdown: false,
     }
   },
   computed: {
     ...mapGetters('twitter', [
       'profile',
+      'newNotifications',
     ]),
   },
   methods: {
