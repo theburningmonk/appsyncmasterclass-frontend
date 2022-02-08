df04d7d5ebbe6136e9fecee151d5f82388ce6c1b
Gerard Sans <gerard.sans@gmail.com>
2022-02-08 11:03:48 +0000

    Add messages route

--- src/router/index.js
+++ src/router/index.js
@@ -72,6 +72,16 @@ const routes = [
     meta: { protected: true }
   },
   {
+    path: '/messages',
+    name: 'Messages',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "messages" */ '../views/Messages.vue'),
+    props: true,
+    meta: { protected: true }
+  },
+  {
     path: '/:screenName',
     name: 'Profile',
     // route level code-splitting
